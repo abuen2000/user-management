248e2ea346454b37e6d4acbdad71a932a49f4781
Arlie Buen <arliebuen@Arl.local>
2023-09-06 15:02:57 +0800

    Initial commit

---
+++ src/test/java/com/usermanageent/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usermanageent.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
